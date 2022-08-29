6f343969cfce45844e25bb6026771505ce3bb4b2
Avery-Monson <111770720+Avery-Monson@users.noreply.github.com>
2022-08-29 21:57:41 +0000

    Mon, Aug 29, 2022, 9:57 PM +00:00

--- hello.c
+++ hello.c
@@ -5,7 +5,8 @@ int main(int argc, char **argv) {
 
   printf("Ariana Shelton\n");
   printf("Computer Science\n");
-
+  printf("Avery Monson\n");
+  printf("Computer Engineering\n");
 
   return 0;
 }
