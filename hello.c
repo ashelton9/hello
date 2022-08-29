8c27b43e9925b8fa41f41ebd7aa678a4238f826a
Avery-Monson <111770720+Avery-Monson@users.noreply.github.com>
2022-08-29 22:41:13 +0000

    'added my name and major to output'

--- hello.c
+++ hello.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv) {
   printf("Ariana Shelton\n");
   printf("Computer Science\n");
   printf("Avery Monson\n");
-  printf("Computer Engineering\n");
+  printf("Computer Engineering!\n");
 
   return 0;
 }
